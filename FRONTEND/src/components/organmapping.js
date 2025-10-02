80ed9dfbbd4fbd1b50ed71688cfb7bc24bdb262d
Vishal Shingala <vishalshingala1826@gmail.com>
2025-10-02 15:22:34 +0530

    feat: Add organ mapping and impact data for astronaut model interactions

---
+++ FRONTEND/src/components/organmapping.js
@@ -0,0 +1,13 @@
+
+const organMapping = {
+  Advanced_Crew_Escape_Suit001: "Metabolism & Reproductive System",
+  Advanced_Crew_Escape_Suit001_2: "Skin Bone",
+  Advanced_Crew_Escape_Suit001_4: "Nervous System",
+  Advanced_Crew_Escape_Suit001_6: "Eyes",
+  Advanced_Crew_Escape_Suit001_9: "Skin Bone",
+  Advanced_Crew_Escape_Suit001_10: "Heart",
+  Advanced_Crew_Escape_Suit001_11: "Immune System & Multi-System & General Cellular Effects",
+  Advanced_Crew_Escape_Suit001_13: "Immune System & Multi-System & General Cellular Effects",
+};
+
+export default organMapping;
